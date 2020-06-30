5c8b9c8af2f858ad81d0fbe15782fb411f995eec
justanotherjoke <david.facsko@gmail.com>
2020-06-30 11:10:43 +0200

    Add about page and how to config emmet infos

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,8 @@ class PagesController < ApplicationController
   def home
     #render html: 'Hello World!'
   end
+
+  def about
+    # az app/views/pages alatt letrehozunk home.html.erb fajlt
+  end
 end
